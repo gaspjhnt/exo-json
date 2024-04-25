1436b73bab3a3b60be12eeaf7d6fa5f71cb27d4a
Gaspard <95536893+gaspjhnt@users.noreply.github.com>
2024-04-25 16:33:30 +0200

    first & last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
-export class AppComponent {
-  title = 'exo-json';
+
+
+export class AppComponent implements OnInit {
+  users: [] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const url = 'https://dummyjson.com/users';
+    this.http.get<any>(url).subscribe((response) => {
+      this.users = response.users.map((user: any) => ({
+        firstName: user.firstName,
+        lastName: user.lastName
+      }));
+    });
+  }
 }
